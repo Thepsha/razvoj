4f717d8fef1b724e4e82079ac01290d0e9b81529
Thepsha <63015462+Thepsha@users.noreply.github.com>
2023-12-06 14:48:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-    
+document.addEventListener('DOMContentLoaded', function () {
+    // Dodajte animacijsku klasu kada se stranica učita
+    var firstImageContainer = document.getElementById('first-image-container');
+    var imageText = document.querySelector('.image-text');
+
+    // Dodajte klasu za fade-in teksta kada se stranica učita
+    imageText.classList.add('text-fade-in');
+
+    // Uklonite klasu za fade-in kada animacija završi
+    imageText.addEventListener('transitionend', function () {
+        imageText.classList.remove('text-fade-in');
+        firstImageContainer.classList.add('is-visible');
+    });
+});
